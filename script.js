6f6b494c88da6337bf19ef3377edc33f97889925
AlexBlejan <alexandrublejan@yahoo.com>
2024-04-01 18:53:28 +0300

    Display first question

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 class Quiz {
-    constructor(questions){}
+	constructor(questions) {
+		this.questions = questions;
+		this.currentQuestionIndex = 0;
+		this.questionContainer = document.getElementById(`question-container`);
+		this.answerButtons = document.getElementById(`answer-buttons`);
+		this.feedback = document.getElementById(`feedback`);
+		this.nextButton = document.getElementById(`next-button`);
+		this.showQuestion(this.questions[0]);
+	}
+	showQuestion(question) {
+		this.questionContainer.innerText = question.question;
+		this.answerButtons.innerHTML = ``;
+		for (const answear of question.answear) {
+			// const answearElement= `<li><button class="btn"></button></li>`
+			const buttonContainer = document.createElement(`li`);
+			const buttonElement = document.createElement(`button`);
+			buttonElement.classList.add(`btn`);
+			buttonElement.textContent = answear.text;
+			this.answerButtons.appendChild(buttonContainer);
+			buttonContainer.appendChild(buttonElement);
+		}
+	}
 }
+
+async function initQuizz() {
+	const fileContent = await fetch("./questions.json");
+	const quizQuestions = await fileContent.json;
+	console.log(quizQuestions);
+	const quiz = new Quiz(questions);
+}
+
+initQuizz();
